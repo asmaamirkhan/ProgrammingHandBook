b9e9c3c9729e37c875d1fcbf52171c7207e261a5
Enver <39167353+capellac@users.noreply.github.com>
2019-10-13 10:56:52 +0300

    Update BuyukSayiyiBulmak.cpp

--- 4-BüyükSayıyıBulmak/BuyukSayiyiBulmak.cpp
+++ 4-BüyükSayıyıBulmak/BuyukSayiyiBulmak.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main() {
 
 	cout << "10 tane sayi giriniz : ";
-	int enBuyuk{};
+	int enBuyuk;
 	cin >> enBuyuk;			//İlk girilen sayıyı direkt en büyük farzediyoruz.
 	for (int i = 0; i < 9; ++i) {	//Kalan 9 girileccek sayıdan şayet herhangi birisi bizim farzettiğimizden büyükse,
-		int sayi{};					
+		int sayi;					
 		cin >> sayi;
 		if (sayi > enBuyuk)
 			enBuyuk = sayi;			//enBuyuk olarak kabul ediliyor.
