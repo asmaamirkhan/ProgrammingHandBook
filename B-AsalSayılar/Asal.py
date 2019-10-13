b5b4a81af67b5dd361777b91ae147103ff03f168
RgSware <54364449+RgSware@users.noreply.github.com>
2019-10-13 13:57:58 +0300

    Python kodları eklendi🐍

---
+++ B-AsalSayılar/Asal.py
@@ -0,0 +1,20 @@
+# 11’den 99’a kadar döngünün oluşturulması.
+for i in range(11,100):
+    # “x”e yeni değerinin atanması
+    x = 2
+    j = 0
+    # “x” ile “i” eşit olmadığı sürece:
+    # döngü devam edecek
+    while (x != i):
+        # “x”in “i”nin bir böleni olup olmadığının kontrol edilmesi
+        if (i%x == 0):
+            j =1
+            # En az bir bölen bulunursa sayının asal olmadığını öğrenmek için yeterlidir
+            break
+        else:
+            x += 1
+
+    if (j == 0):
+        # Asal sayıların ekrana yazdırılması
+        print(i)
+
